cfcb312fa3bf8f374a6cd23bbb771caaeca8bd56
kiran <kiran.s.dhawan@gmail.com>
2025-07-08 14:31:49 +0530

    Port added

--- backend/main.py
+++ backend/main.py
@@ -63,3 +63,7 @@ async def root():
     return {"message": "Hello World"}
 
 
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
